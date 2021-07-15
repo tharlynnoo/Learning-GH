0979e96595d8fdbfbd6a9a4fe9e466d47a734bfb
tharlynnoo <tharlynnoo@gmail.com>
2021-07-15 16:39:24 +0630

    Initial Commit

---
+++ LearningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGH
+//
+//  Created by Thar Lynn Oo on 7/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Thar Lynn Oo on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
